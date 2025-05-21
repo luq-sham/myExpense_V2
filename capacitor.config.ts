d4e1f710c4caf087496d83709a069e9dc9693acd
luq-sham <luqman.shamsul10@gmail.com>
2025-05-21 22:11:30 +0800

    update 2

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,20 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'myexpense',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      lSplashScreen: {
+        launchShowDuration: 3000,          // Duration in ms (e.g. 3000 = 3 seconds)
+        launchAutoHide: true,              // Hide automatically after duration
+        showSpinner: false,                // Optional: show/hide spinner
+        backgroundColor: "#264653",        // Background color (HEX format)
+        androidScaleType: "CENTER_CROP",   // Options: FIT_CENTER, CENTER_CROP, etc.
+        splashFullScreen: false,           // Full screen or not
+        splashImmersive: false             // Hide system UI (e.g. nav/status bar)
+      }
+    }
+  }
 };
 
 export default config;
